ca19a5d3c19a1e2c3dd6080c38f2e29b93ec60c6
Nauman <nauman33183@gmail.com>
2026-04-14 12:25:10 +0500

    frontend url

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,10 @@ const PORT = process.env.PORT;
 const app: Express = express();
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  origin: String(process.env.FRONTEND_URL) || [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+  ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
   credentials: true,
   exposedHeaders: ["Content-Disposition"],
